5cbd786bb968cd72b048543dbbe8b42b22e6c753
Ned Stickler <nedstickler@gmail.com>
2024-02-13 13:14:42 +0000

    Refactor load_data to allow for any path, add docstring

--- src/load_data.py
+++ src/load_data.py
@@ -1,11 +1,19 @@
 
 import numpy as np
 import tensorflow_datasets as tfds
-from tensorflow_datasets.core.dataset_utils import _IterableDataset
+from pathlib import Path
 
 
-def load_data(venv_path: str) -> _IterableDataset:
-    dataset = tfds.load("resisc45", data_dir=f"{venv_path}\\Lib\\site-packages\\tensorflow_datasets\\")
+def load_data(package_path: str | Path) -> np.array:
+    """Load the RESICS45 dataset using tensorflow_datasets.
+    
+    Args:
+        package_path: Path to the 'tensorflow_datasets' package (./venv/Lib/site-packages/tensorflow_datasets/).
+
+    Returns:
+        A numpy array containing the dataset with shape (31_500, 256, 256, 3).
+    """
+    dataset = tfds.load("resisc45", data_dir=package_path)
     dataset_as_numpy = tfds.as_numpy(dataset)
     images = np.array([feature.get("image") for feature in dataset_as_numpy.get("train")])
     dataset_as_numpy = None
