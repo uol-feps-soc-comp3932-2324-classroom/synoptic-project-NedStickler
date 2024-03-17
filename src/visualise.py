32f666cc2c4035445c26dd7588a749b1afbf9093
Ned Stickler <nedstickler@gmail.com>
2024-03-17 17:51:43 +0000

    Rename files

--- src/visualise.py
+++ src/visualise.py
@@ -5,9 +5,10 @@ from utils import visualise_generator
 
 
 if __name__ == "__main__":
+    generator_path = r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\generators\srresnet-mse\srresnet-mse-e1-resics45.keras"
     downsample_factor = 4
     hr_dataset = np.load(r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\datasets\resics45_s2048.npy")
     lr_dataset = lr_dataset = np.array([image[::downsample_factor, ::downsample_factor, :] for image in hr_dataset])
 
-    generator = keras.saving.load_model(r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\generators\srresnet-mse\srresnet-mse-e1-resics45.keras")
+    generator = keras.saving.load_model(generator_path)
     visualise_generator(generator, lr_dataset[:5], hr_dataset[:5])
