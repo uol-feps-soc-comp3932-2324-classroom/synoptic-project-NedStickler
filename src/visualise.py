0e0759968cac4456d689e79684470619d9206459
Ned Stickler <nedstickler@gmail.com>
2024-03-19 00:26:40 +0000

    Add training for SRGAN

--- src/visualise.py
+++ src/visualise.py
@@ -5,7 +5,7 @@ from utils import visualise_generator
 
 
 if __name__ == "__main__":
-    generator_path = r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\generators\srresnet-mse\srresnet-mse-e1-resics45.keras"
+    generator_path = r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\generators\srresnet-mse\srresnet-mse-e100-resics45.keras"
     downsample_factor = 4
     hr_dataset = np.load(r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\datasets\resics45_s2048.npy")
     lr_dataset = lr_dataset = np.array([image[::downsample_factor, ::downsample_factor, :] for image in hr_dataset])
