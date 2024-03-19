1211746e801dc828f94c0f3e87bda315a20c1b37
Ned Stickler <nedstickler@gmail.com>
2024-03-19 23:23:43 +0000

    Implement training second pass for SRGAN VGG22

--- src/training.py
+++ src/training.py
@@ -6,19 +6,21 @@ from utils import GANSaver
 import paths
 
 
-def train_srresnet_mse():
+def train_srresnet_mse() -> None:
     save_checkpoint = ModelCheckpoint(paths.SAVE_PATH + f"/srresnet-mse/srresnet-mse-e{epochs}-resics45.keras", monitor="loss", save_best_only=True, mode="auto", save_freq="epoch")
     srresnet = SRResNet(16)
     srresnet.compile(optimizer=keras.optimizers.Adam(learning_rate=10**-4), loss=keras.losses.MeanSquaredError())
     srresnet.fit(lr_dataset, hr_dataset, epochs=epochs, callbacks=[save_checkpoint])
     
 
-def train_srgan_vgg22(first_pass: bool = True):
+def train_srgan_vgg22(first_pass: bool, discriminator_path: str = None, generator_path: str = None) -> None:
     if first_pass:
         discriminator = None
+        generator = keras.saving.load_model(paths.REPO_PATH + "/generators/srresnet-mse/srresnet-mse-e1000-resics45.keras")
         lr = 10**-4
     else:
-        discriminator = keras.saving.load_model()
+        discriminator = keras.saving.load_model(discriminator_path)
+        generator = keras.saving.load_model(generator_path)
         lr = 10**-5
 
     details["lr"] = lr
@@ -27,8 +29,7 @@ def train_srgan_vgg22(first_pass: bool = True):
     vgg = keras.Model(vgg.input, vgg.layers[5].output)
 
     # Change the next line manually to switch pre-trained generators
-    srresnet = keras.saving.load_model(paths.REPO_PATH + "/generators/srresnet-mse/srresnet-mse-e1000-resics45.keras")
-    srgan = SRGAN(generator=srresnet, vgg=vgg, discriminator=discriminator)
+    srgan = SRGAN(generator=generator, vgg=vgg, discriminator=discriminator)
     srgan.compile(d_optimiser=keras.optimizers.Adam(learning_rate=lr), g_optimiser=keras.optimizers.Adam(learning_rate=lr))
     srgan.fit(lr_dataset, hr_dataset, epochs=epochs, callbacks=[gan_saver])
 
@@ -48,4 +49,6 @@ if __name__ == "__main__":
     if model.lower() == "srresnet-mse":
         train_srresnet_mse()
     if model.lower() == "srgan-vgg22":
-        train_srgan_vgg22(first_pass=True)
+        discriminator_path = paths.REPO_PATH + "/generators/srgan-vgg22/srgan-vgg22-e100-lr0.0001-resics45/discriminator.keras"
+        generator_path = paths.REPO_PATH + "/generators/srgan-vgg22/srgan-vgg22-e100-lr0.0001-resics45/generator.keras"
+        train_srgan_vgg22(first_pass=False, discriminator_path=discriminator_path, generator_path=generator_path)
--- src/visualise.py
+++ src/visualise.py
@@ -1,18 +1,15 @@
 import keras
 import numpy as np
-from models import SRGAN, SRResNet
+from models import SRGAN
 from utils import visualise_generator
+from layers import PixelShuffle
 
 
 if __name__ == "__main__":
-    model = "srgan"
-    epochs = 100
-    SRResNet(16)
-
-    generator_path = f"C:\\Users\\nedst\\Desktop\\synoptic-project-NedStickler\\generators\\{model}\\{model}-e{epochs}-0.001-resics45.keras"
+    generator_path = r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\generators\srgan-vgg22\srgan-vgg22-e100-lr0.0001-resics45\generator.keras"
     downsample_factor = 4
     hr_dataset = np.load(r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\datasets\resics45_s2048.npy")
     lr_dataset = lr_dataset = np.array([image[::downsample_factor, ::downsample_factor, :] for image in hr_dataset])
 
     generator = keras.saving.load_model(generator_path)
-    visualise_generator(generator, lr_dataset[:5], hr_dataset[:5])
+    visualise_generator(generator, lr_dataset[5:10], hr_dataset[5:10])
