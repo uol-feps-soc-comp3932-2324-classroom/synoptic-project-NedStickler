92cfd1538535cd93fa9e2a748a95ca1a19403419
Ned Stickler <nedstickler@gmail.com>
2024-03-19 10:42:56 +0000

    Add SRGAN training

--- src/training.py
+++ src/training.py
@@ -17,19 +17,16 @@ def train_srgan(epochs, save_path):
     vgg = keras.applications.VGG19(input_shape=(None, None, 3), weights="imagenet", include_top=False)
     vgg = keras.Model(vgg.input, vgg.layers[20].output)
 
-    # TODO:
-    # Fix this! Generator needs to be pretrained srresnet
-
     srresnet = keras.saving.load_model("/tmp/sc20ns/generators/srresnet-mse/srresnet-mse-e100-resics45.keras")
-    srgan = SRGAN(residual_blocks=16, vgg=vgg)
+    srgan = SRGAN(generator=srresnet, vgg=vgg)
     srgan.compile(d_optimiser=keras.optimizers.Adam(learning_rate=0.0003), g_optimiser=keras.optimizers.Adam(learning_rate=0.0003))
     srgan.fit(lr_dataset, hr_dataset, epochs=epochs, callbacks=[gan_saver])
 
 
 if __name__ == "__main__":
-    model = "srresnet-mse"
+    model = "srgan"
 
-    epochs = 2
+    epochs = 3
     save_path = f"/tmp/sc20ns/generators/{model}/{model}-e{epochs}-resics45.keras"
     downsample_factor = 4
 
