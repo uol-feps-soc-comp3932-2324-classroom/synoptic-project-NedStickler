ac23dea8eec428625f9f81827b8e580f8a3d3f23
Ned Stickler <nedstickler@gmail.com>
2024-03-28 16:05:58 +0100

    Potential fix for SymbolicTensor

--- src/utils.py
+++ src/utils.py
@@ -45,11 +45,10 @@ def crop_and_resize_image(image: np.array, downsample_factor: int) -> keras.Mode
 
 
 def crop_and_resize_batch(batch: np.array, downsample_factor: int) -> np.array:
-    lr_images = [] 
+    lr_images = []
     hr_images = []
-    for image in batch:
-        for _ in range(16):
-            lr_image, hr_image = crop_and_resize_image(image, downsample_factor)
-            lr_images.append(lr_image)
-            hr_images.append(hr_image)
-    return np.array(lr_images), np.array(hr_images)
+    for _ in range(16):
+        lr_image, hr_image = crop_and_resize_image(batch, downsample_factor)
+        lr_images.append(lr_image)
+        hr_images.append(hr_image)
+    return np.concatenate(lr_images), np.concatenate(hr_images)
