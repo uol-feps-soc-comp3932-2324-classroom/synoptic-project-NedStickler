22f5afc4d6b1add432d92903714c2b8ba8581a4f
Ned Stickler <nedstickler@gmail.com>
2024-03-29 10:29:07 +0100

    Remove legacy SRResNet

--- src/models.py
+++ src/models.py
@@ -8,43 +8,6 @@ from layers import PixelShuffle
 from keras.layers import RandomCrop, Resizing
 
 
-# def SRResNet(residual_blocks: int) -> keras.Model:
-#     def g_residual_block(x_in):
-#         x = layers.Conv2D(64, kernel_size=3, padding="same")(x_in)
-#         x = layers.BatchNormalization()(x)
-#         x = layers.PReLU(shared_axes=[1, 2])(x)
-#         x = layers.Conv2D(64, kernel_size=3, padding="same")(x)
-#         x = layers.BatchNormalization()(x)
-#         x = layers.Add()([x_in, x])
-#         return x
-    
-#     # LR input
-#     inputs = layers.Input((None, None, 3))
-#     x_in = layers.Rescaling(scale=1/255)(inputs)
-#     # First convolution
-#     x_in = layers.Conv2D(64, kernel_size=3, padding="same")(x_in)
-#     x_in = x = layers.PReLU(shared_axes=[1, 2])(x_in)
-
-#     # Residual block set
-#     for _ in range(residual_blocks):
-#         x = g_residual_block(x)
-
-#     # Residual block without activation functions
-#     x = layers.Conv2D(64, kernel_size=3, padding="same")(x)
-#     x = layers.BatchNormalization()(x)
-#     x = layers.Add()([x_in, x])
-#     # Upscaling blocks
-#     x = layers.Conv2D(256, kernel_size=3, padding="same")(x)
-#     x = PixelShuffle()(x)
-#     x = layers.PReLU(shared_axes=[1, 2])(x)
-#     x = layers.Conv2D(256, kernel_size=3, padding="same")(x)
-#     x = PixelShuffle()(x)
-#     x = layers.PReLU(shared_axes=[1, 2])(x)
-#     # Final convolve
-#     x = layers.Conv2D(3, kernel_size=3, padding="same")(x)
-#     x = layers.Rescaling(scale=127.5, offset=127.5)(x)
-#     return keras.Model(inputs, x)
-
 @keras.saving.register_keras_serializable()
 class CropAndResize(keras.Model):
     def __init__(self, downsample_factor):
