dae43958c6b66f1005d1cb510c7d81d23f6b1258
Ned Stickler <sc20ns@leeds.ac.uk>
2024-04-03 14:20:23 +0100

    Another another config test

--- src/layers.py
+++ src/layers.py
@@ -7,5 +7,5 @@ class PixelShuffle(keras.Layer):
     def call(self, x):
         return tf.nn.depth_to_space(x, 2)
     
-    def build(self):
-        super().build()
+    def build(self, input_shape):
+        super().build(input_shape)
