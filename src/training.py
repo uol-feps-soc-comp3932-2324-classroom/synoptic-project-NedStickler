556bc6086ae579f1febe46dc11b9829f080a2985
Ned Stickler <sc20ns@leeds.ac.uk>
2024-04-03 10:24:56 +0100

    VGG training skeleton

--- src/training.py
+++ src/training.py
@@ -51,7 +51,7 @@ class Training():
         save_checkpoint = ModelCheckpoint(paths.SAVE_PATH + f"/vgg/vgg-e{self.epochs}-resics45.keras", monitor="loss", save_best_only=True, mode="auto", save_freq="epoch")
         vgg = keras.applications.VGG19(input_shape=(256, 256, 3), weights=None, classes=45)
         vgg.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss=keras.losses.CategoricalCrossentropy())
-        vgg.fit(self.dataset, keras.utils.to_categorical(self.labels), epochs=self.epochs, callbacks=[save_checkpoint])
+        vgg.fit(keras.applications.vgg19.preprocess_input(self.dataset), keras.utils.to_categorical(self.labels), epochs=self.epochs, callbacks=[save_checkpoint])
     
     def train(self) -> None:
         if self.model == "srresnet-mse":
@@ -69,5 +69,5 @@ class Training():
 
 
 if __name__ == "__main__":
-    training = Training(model="vgg", epochs=5, patch=True)
+    training = Training(model="vgg", epochs=1409, patch=True)
     training.train()
