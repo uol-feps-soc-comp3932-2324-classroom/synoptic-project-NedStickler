4b38a0a2748d363052ea00d546860074a093729d
Ned Stickler <nedstickler@gmail.com>
2024-04-08 19:26:23 +0100

    Additions to training procedure

--- src/visualise.py
+++ src/visualise.py
@@ -4,14 +4,12 @@ from keras.layers import Resizing
 import numpy as np
 from models import SRGAN, SRResNet
 from utils import visualise_generator
-from layers import GaussianBlur
 
 
 if __name__ == "__main__":
-    generator_path = r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\generators\srgan-vgg54\srgan-vgg54-e67-lr1e-05-resics45\generator.keras"
+    generator_path = r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\generators\srgan-vgg22\srgan-vgg22-e159-lr0.0001-resics45\generator.keras"
     hr_images = np.load(r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\datasets\resisc45_test.npy")
-    lr_images = GaussianBlur()(hr_images)
-    lr_images = Resizing(256 // 4, 256 // 4, interpolation="bicubic")(lr_images).numpy().astype(np.uint8)
+    lr_images = Resizing(256 // 4, 256 // 4, interpolation="bicubic")(hr_images).numpy().astype(np.uint8)
 
     generator = keras.saving.load_model(generator_path)
     visualise_generator(generator, lr_images[:3], hr_images[:3])
