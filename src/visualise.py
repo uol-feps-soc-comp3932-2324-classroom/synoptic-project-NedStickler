cf84583fb076b86a04f3de774320d9c2b2b1cca1
Ned Stickler <nedstickler@gmail.com>
2024-04-15 01:02:01 +0100

    Restructure and continuation of methods

--- src/visualise.py
+++ src/visualise.py
@@ -44,7 +44,7 @@ class BlurAndResize(keras.Model):
 
 
 if __name__ == "__main__":
-    generator_path = r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\generators\srgan-vgg54\srgan-vgg54-e159-lr1e-05-resics45\generator.keras"
+    generator_path = r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\generators\srgan-vgg22\srgan-vgg22-e159-lr1e-05-resics45\generator.keras"
     hr_images = np.load(r"C:\Users\nedst\Desktop\synoptic-project-NedStickler\datasets\resisc45_test.npy")
     lr_images = BlurAndResize(4)(hr_images).numpy().astype(np.uint8)
     generator = keras.saving.load_model(generator_path)
